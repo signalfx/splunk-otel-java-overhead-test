2a719ea44b3df89ce690e6d4a767e83b57cd1012
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-04-28 13:01:01 +0200

    [agent-version-update] Update agent version to 1.10.1

--- src/test/java/io/opentelemetry/agents/AgentVersion.java
+++ src/test/java/io/opentelemetry/agents/AgentVersion.java
@@ -7,7 +7,7 @@ package io.opentelemetry.agents;
 public final class AgentVersion {
 
   // @formatter:off
-  static final String LATEST_VERSION = "1.10.0";
+  static final String LATEST_VERSION = "1.10.1";
   // @formatter:on
 
   private AgentVersion() {
