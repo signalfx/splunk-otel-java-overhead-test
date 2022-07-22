3a9d941204391d17f4781fbde44520ff4a8f1d4b
Jason Plumb <jplumb@splunk.com>
2022-07-22 08:22:01 -0700

    use key id directly

--- provision/create-ui-pr.sh
+++ provision/create-ui-pr.sh
@@ -24,11 +24,10 @@ gpg --batch --allow-secret-key-import --import /tmp/sk
 rm /tmp/sk
 
 echo ">>> Setting up git config options"
-GPG_KEY_ID=$(gpg2 -K --keyid-format SHORT | grep '^ ' | tr -d ' ')
 git config --global user.name overhead-results
 git config --global user.email ssg-srv-gh-o11y-gdi@splunk.com
 git config --global gpg.program gpg
-git config --global user.signingKey ${GPG_KEY_ID}
+git config --global user.signingKey "${GITHUB_BOT_GPG_KEY_ID}"
 
 git clone https://srv-gh-o11y-gdi:"${GITHUB_TOKEN}"@github.com/signalfx/splunk-otel-java-overhead-test.git github-clone
 cd github-clone
